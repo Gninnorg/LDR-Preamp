16ec2e07ae4459e02b09d4852be60cc84c11abfa
Gninnorg <38350314+Gninnorg@users.noreply.github.com>
2019-02-28 18:35:55 +0100

    Add files via upload

---
+++ ads1115/ADS1115.cpp
@@ -0,0 +1,513 @@
+/**************************************************************************/
+/*!
+    @file     ADS1115.cpp
+    @author   Carsten Grønning
+    @license  BSD (see license.txt)
+
+    Inspired by the Adafruit_ADS1015 library
+
+    Driver for the ADS1115 ADC
+
+    @section  HISTORY
+
+    v1.0 - First release
+*/
+
+/**************************************************************************/
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+#include "ADS1115.h"
+
+
+/**************************************************************************
+    Instantiates a new ADS1115 class with an ADS1115 address and a
+    selected programable gain.
+    Default address: ADDRESS_GND
+    Default PGA: PGA_6_144V
+/**************************************************************************/
+ADS1115::ADS1115(ads1115_I2C_address_t i2cAddress, ads1115_conf_pga_t config_pga)
+{
+   m_i2cAddress = i2cAddress;
+   m_config_pga = config_pga;
+}
+
+
+/**************************************************************************
+    Start Wire for for i2c communication
+/**************************************************************************/
+void ADS1115::begin()
+{
+  Wire.begin();
+}
+
+/**************************************************************************
+    Gets a single-ended ADC reading from AIN0 to GND
+/**************************************************************************/
+int16_t ADS1115::getCountA0Gnd()
+{
+  setMultiplexer(MUX_SINGLE_0);
+  return getConversion();
+}
+
+/**************************************************************************
+    Gets a single-ended ADC reading from AIN1 to GND
+/**************************************************************************/
+int16_t ADS1115::getCountA1Gnd()
+{
+  setMultiplexer(MUX_SINGLE_1);
+  return getConversion();
+}
+
+/**************************************************************************
+    Gets a single-ended ADC reading from AIN2 to GND
+/**************************************************************************/
+int16_t ADS1115::getCountA2Gnd()
+{
+  setMultiplexer(MUX_SINGLE_2);
+  return getConversion();
+}
+
+/**************************************************************************
+    Gets a single-ended ADC reading from AIN3 to GND
+/**************************************************************************/
+int16_t ADS1115::getCountA3Gnd()
+{
+  setMultiplexer(MUX_SINGLE_3);
+  return getConversion();
+}
+
+/**************************************************************************
+    Gets a differential ADC reading from AIN0 to AIN1
+/**************************************************************************/
+int16_t ADS1115::getCountA0A1()
+{
+  setMultiplexer(MUX_DIFF_0_1);
+  return getConversion();
+}
+
+/**************************************************************************
+    Gets a differential ADC reading from AIN0 to AIN3
+/**************************************************************************/
+int16_t ADS1115::getCountA0A3()
+{
+  setMultiplexer(MUX_DIFF_0_3);
+  return getConversion();
+}
+
+/**************************************************************************
+    Gets a differential ADC reading from AIN1 to AIN3
+/**************************************************************************/
+int16_t ADS1115::getCountA1A3()
+{
+  setMultiplexer(MUX_DIFF_1_3);
+  return getConversion();
+}
+
+/**************************************************************************
+    Gets a differential ADC reading from AIN1 to AIN3
+/**************************************************************************/
+int16_t ADS1115::getCountA2A3()
+{
+  setMultiplexer(MUX_DIFF_2_3);
+  return getConversion();
+}
+
+/**************************************************************************
+    Trigger a conversion and wait for conversion to complete
+/**************************************************************************/
+int16_t ADS1115::getConversion()
+{
+  triggerConversion();
+
+  if(pollConversion((uint8_t)1000)) {
+    return getLastConversionResults();
+  } else {
+    return 0;
+  }
+}
+
+/**************************************************************************
+    Utility to get millivolt pr. ADC count for at the configured PGA
+/**************************************************************************/
+float ADS1115::getMvPerCount()
+{
+  switch (m_config_pga)
+  {
+    case (PGA_6_144V):
+      return 0.187500;
+      break;
+    case (PGA_4_096V):
+      return 0.125000;
+      break;
+    case (PGA_2_048V):
+      return 0.062500;
+      break;
+    case (PGA_1_024V):
+      return 0.031250;
+      break;
+    case (PGA_0_512V):
+      return 0.015625;
+      break;
+    case (PGA_0_256V):
+      return 0.007813;
+      break;
+  }
+}
+
+/**************************************************************************
+    Utility to convert ADC count to Volt
+/**************************************************************************/
+float ADS1115::convertCountToVolt(int16_t count)
+{
+  return count * getMvPerCount() / 1000;
+}
+
+/**************************************************************************
+    Trigger til ADC1115 to do a single shot reading
+/**************************************************************************/
+void ADS1115::triggerConversion()
+{
+  uint16_t config = getConfiguration();
+  config |= OS_SET_SINGLE_CONV;
+  writeRegister(REG_CONFIG, config);
+}
+
+/**************************************************************************
+    Check if the ADC1115 has finished the conversion
+/**************************************************************************/
+boolean ADS1115::isConversionReady()
+{
+  return readRegister(REG_CONFIG)>>15;
+}
+
+/**************************************************************************
+    Wait for conversion to finish by retrying to check if conversion is
+    ready. Function will continue to check until max_retries is reached.
+/**************************************************************************/
+boolean ADS1115::pollConversion(uint8_t max_retries)
+{
+  for(uint16_t i = 0; i < max_retries; i++) {
+    if (isConversionReady()) return true;
+  }
+  return false;
+}
+
+/**************************************************************************
+    Gets a last conversion from the ADC1115
+/**************************************************************************/
+int16_t ADS1115::getLastConversionResults()
+{
+  return (int16_t) readRegister(REG_CONVERSION);
+}
+
+/**************************************************************************
+    Sets the datarate for the ADC1115 -
+    the setting only affect continuous readings
+/**************************************************************************/
+void ADS1115::setDataRate(ads1115_conf_dr_t config_dr)
+{
+  m_config_dr = config_dr;
+}
+
+/**************************************************************************
+    Sets the selected input settings for the ADS1115
+/**************************************************************************/
+void ADS1115::setMultiplexer(ads1115_conf_mux_t config_mux)
+{
+  m_config_mux = config_mux;
+}
+
+/**************************************************************************
+    Builds and returns a 16 bit setting - used for setting up the ADS1115
+/**************************************************************************/
+uint16_t ADS1115::getConfiguration()
+{
+  uint16_t config = m_config_os     |
+                    m_config_mux    |
+                    m_config_pga    |
+                    m_config_mode   |
+                    m_config_dr     |
+                    m_config_cmode  |
+                    m_config_cpol   |
+                    m_config_clat   |
+                    m_config_cque;
+  return config;
+}
+
+/**************************************************************************
+    Gets a single-ended ADC reading from AIN0 to GND
+/**************************************************************************/
+uint16_t ADS1115::readRegister(ads1115_reg_pointer_t reg)
+{
+  Wire.beginTransmission(m_i2cAddress);
+  // Select the register from which you want to read from
+  Wire.write((uint8_t)reg);
+  Wire.endTransmission();
+
+  // Read 2 bytes form register and return the register
+  Wire.requestFrom(m_i2cAddress, (uint8_t)2);
+  return ((Wire.read() << 8) | Wire.read());
+}
+
+/**************************************************************************
+    Write 16 bit to a selected ADC1115 register
+**************************************************************************/
+void ADS1115::writeRegister(ads1115_reg_pointer_t reg, uint16_t value)
+{
+  Wire.beginTransmission(m_i2cAddress);
+  Wire.write((uint8_t)reg);
+  Wire.write((uint8_t)(value>>8));
+  Wire.write((uint8_t)(value & 0xFF));
+  Wire.endTransmission();
+}
+
+/**************************************************************************
+    Utility to calculate R2 (Ohm) in a voltage divider from a given input,
+    output and R1
+/**************************************************************************/
+uint16_t  ADS1115::getR2ValueVoltageDivider(float input, float output, uint16_t r1){
+  return (uint16_t)((r1 / (1 - (output / input)))-r1);
+}
+
+/**************************************************************************
+    Utility to calculate R1 (Ohm) in a voltage divider from a given input,
+    output and R2
+/**************************************************************************/
+uint16_t  ADS1115::getR1ValueVoltageDivider(float input, float output, uint16_t r2){
+  return (uint16_t)((r2 / ((output / input)))-r2);
+}
+
+/**************************************************************************
+    Utility to calculate shunt value for a pontentiometer of a given size
+    by providing af pot_size of 1 the function will return the series
+    relative value in percent of a given pontentiometer
+/**************************************************************************/
+float ADS1115::getShuntForAttenuation(double attenuation, uint16_t pot_size)
+{
+  if (attenuation < 0) {
+    return pow(10,(attenuation/20)) * pot_size;
+  } else {
+    return 0;
+  }
+}
+
+/**************************************************************************
+    Utility to calculate shunt value for a pontentiometer of a given size
+    by providing af pot_size of 1 the function will return the shunts
+    relative value in percent of a given pontentiometer
+/**************************************************************************/
+float ADS1115::getSerieForAttenuation(double attenuation, uint16_t pot_size )
+{
+  if (attenuation < 0) {
+    return (1 - pow(10,(attenuation/20))) * pot_size;
+  } else {
+    return 0;
+  }
+}
+
+/**************************************************************************
+    Utility to output the current settings of the ADC1115 object to the
+    Serial object
+/**************************************************************************/
+void ADS1115::showConfigRegister()
+{
+    #ifdef ADS1115_SERIAL_DEBUG
+    uint16_t configRegister = getConfiguration();
+    Serial.print("Configuration file is:");
+    Serial.println(configRegister,BIN);
+
+    configRegister = readRegister(REG_CONFIG);
+    Serial.print("Device configuration file is:");
+    Serial.println(configRegister,BIN);
+    #endif
+}
+
+/**************************************************************************/
+/*!
+    @brief  Reads the conversion results, measuring the voltage
+            difference between the P (AIN0) and N (AIN1) input.  Generates
+            a signed value since the difference can be either
+            positive or negative.
+*/
+/**************************************************************************
+
+int16_t ADS1115::readADC_Differential_0_1() {
+  // Start with default values
+  uint16_t config = ADS1115_REG_CONFIG_CQUE_NONE    | // Disable the comparator (default val)
+                    ADS1115_REG_CONFIG_CLAT_NONLAT  | // Non-latching (default val)
+                    ADS1115_REG_CONFIG_CPOL_ACTVLOW | // Alert/Rdy active low   (default val)
+                    ADS1115_REG_CONFIG_CMODE_TRAD   | // Traditional comparator (default val)
+                    ADS1115_REG_CONFIG_DR_128SPS    | // 1600 samples per second (default)
+                    ADS1115_REG_CONFIG_MODE_SINGLE;   // Single-shot mode (default)
+
+  // Set PGA/voltage range
+  config |= m_gain;
+
+  // Set channels
+  config |= ADS1115_REG_CONFIG_MUX_DIFF_0_1;          // AIN0 = P, AIN1 = N
+
+  // Set 'start single-conversion' bit
+  config |= ADS1115_REG_CONFIG_OS_SINGLE;
+
+  // Write config register to the ADC
+  writeRegister(m_i2cAddress, ADS1115_REG_POINTER_CONFIG, config);
+
+  // Wait for the conversion to complete
+  delay(m_conversionDelay);
+
+  // Read the conversion results
+  uint16_t res = readRegister(m_i2cAddress, ADS1115_REG_POINTER_CONVERT) >> m_bitShift;
+  if (m_bitShift == 0)
+  {
+    return (int16_t)res;
+  }
+  else
+  {
+    // Shift 12-bit results right 4 bits for the ADS1115,
+    // making sure we keep the sign bit intact
+    if (res > 0x07FF)
+    {
+      // negative number - extend the sign to 16th bit
+      res |= 0xF000;
+    }
+    return (int16_t)res;
+  }
+} */
+
+/**************************************************************************/
+/*!
+    @brief  Reads the conversion results, measuring the voltage
+            difference between the P (AIN2) and N (AIN3) input.  Generates
+            a signed value since the difference can be either
+            positive or negative.
+*/
+/**************************************************************************
+int16_t ADS1115::readADC_Differential_2_3() {
+  // Start with default values
+  uint16_t config = ADS1115_REG_CONFIG_CQUE_NONE    | // Disable the comparator (default val)
+                    ADS1115_REG_CONFIG_CLAT_NONLAT  | // Non-latching (default val)
+                    ADS1115_REG_CONFIG_CPOL_ACTVLOW | // Alert/Rdy active low   (default val)
+                    ADS1115_REG_CONFIG_CMODE_TRAD   | // Traditional comparator (default val)
+                    ADS1115_REG_CONFIG_DR_128SPS    | // 1600 samples per second (default)
+                    ADS1115_REG_CONFIG_MODE_SINGLE;   // Single-shot mode (default)
+
+  // Set PGA/voltage range
+  config |= m_gain;
+
+  // Set channels
+  config |= ADS1115_REG_CONFIG_MUX_DIFF_2_3;          // AIN2 = P, AIN3 = N
+
+  // Set 'start single-conversion' bit
+  config |= ADS1115_REG_CONFIG_OS_SINGLE;
+
+  // Write config register to the ADC
+  writeRegister(m_i2cAddress, ADS1115_REG_POINTER_CONFIG, config);
+
+  // Wait for the conversion to complete
+  delay(m_conversionDelay);
+
+  // Read the conversion results
+  uint16_t res = readRegister(m_i2cAddress, ADS1115_REG_POINTER_CONVERT) >> m_bitShift;
+  if (m_bitShift == 0)
+  {
+    return (int16_t)res;
+  }
+  else
+  {
+    // Shift 12-bit results right 4 bits for the ADS1115,
+    // making sure we keep the sign bit intact
+    if (res > 0x07FF)
+    {
+      // negative number - extend the sign to 16th bit
+      res |= 0xF000;
+    }
+    return (int16_t)res;
+  }
+}
+*/
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the comparator to operate in basic mode, causing the
+            ALERT/RDY pin to assert (go from high to low) when the ADC
+            value exceeds the specified threshold.
+
+            This will also set the ADC in continuous conversion mode.
+*/
+/**************************************************************************
+void ADS1115::startComparator_SingleEnded(uint8_t channel, int16_t threshold)
+{
+  // Start with default values
+  uint16_t config = ADS1115_REG_CONFIG_CQUE_1CONV   | // Comparator enabled and asserts on 1 match
+                    ADS1115_REG_CONFIG_CLAT_LATCH   | // Latching mode
+                    ADS1115_REG_CONFIG_CPOL_ACTVLOW | // Alert/Rdy active low   (default val)
+                    ADS1115_REG_CONFIG_CMODE_TRAD   | // Traditional comparator (default val)
+                    ADS1115_REG_CONFIG_DR_128SPS    | // 1600 samples per second (default)
+                    ADS1115_REG_CONFIG_MODE_CONTIN  | // Continuous conversion mode
+                    ADS1115_REG_CONFIG_MODE_CONTIN;   // Continuous conversion mode
+
+  // Set PGA/voltage range
+  config |= m_gain;
+
+  // Set single-ended input channel
+  switch (channel)
+  {
+    case (0):
+      config |= ADS1115_REG_CONFIG_MUX_SINGLE_0;
+      break;
+    case (1):
+      config |= ADS1115_REG_CONFIG_MUX_SINGLE_1;
+      break;
+    case (2):
+      config |= ADS1115_REG_CONFIG_MUX_SINGLE_2;
+      break;
+    case (3):
+      config |= ADS1115_REG_CONFIG_MUX_SINGLE_3;
+      break;
+  }
+
+  // Set the high threshold register
+  // Shift 12-bit results left 4 bits for the ADS1115
+  writeRegister(m_i2cAddress, ADS1115_REG_POINTER_HITHRESH, threshold << m_bitShift);
+
+  // Write config register to the ADC
+  writeRegister(m_i2cAddress, ADS1115_REG_POINTER_CONFIG, config);
+}
+*/
+
+/**************************************************************************/
+/*!
+    @brief  In order to clear the comparator, we need to read the
+            conversion results.  This function reads the last conversion
+            results without changing the config value.
+*/
+/**************************************************************************
+int16_t ADS1115::getLastConversionResults()
+{
+  // Wait for the conversion to complete
+  delay(m_conversionDelay);
+
+  // Read the conversion results
+  uint16_t res = readRegister(m_i2cAddress, ADS1115_REG_POINTER_CONVERT) >> m_bitShift;
+  if (m_bitShift == 0)
+  {
+    return (int16_t)res;
+  }
+  else
+  {
+    // Shift 12-bit results right 4 bits for the ADS1115,
+    // making sure we keep the sign bit intact
+    if (res > 0x07FF)
+    {
+      // negative number - extend the sign to 16th bit
+      res |= 0xF000;
+    }
+    return (int16_t)res;
+  }
+}
+*/
---
+++ ads1115/ADS1115.h
@@ -0,0 +1,208 @@
+/**************************************************************************/
+/*!
+    @file     ADS1115.h
+    @author   Carsten Grønning
+    @license  BSD (see license.txt)
+
+    @section  HISTORY
+
+*/
+/**************************************************************************/
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+
+
+// I2C ADDRESS/BITS
+//-----------------------------------------------------------------------------
+typedef enum
+{
+    ADDRESS_GND     = 0x48,   // 1001 000 (ADDR = GND)
+    ADDRESS_VDD     = 0x49,   // 1001 001 (ADDR = VDD)
+    ADDRESS_SDA     = 0x4A,   // 1001 010 (ADDR = SDA)
+    ADDRESS_SCL     = 0x4B,   // 1001 011 (ADDR = VDD)
+    ADDRESS_DEFAULT = 0x48
+} ads1115_I2C_address_t;
+
+
+// Pointer register
+// ----------------------------------------------------------------------------
+typedef enum
+{
+    REG_CONVERSION  = 0x00,    // 0000 0000 = Conversion register
+    REG_CONFIG      = 0x01,    // 0000 0001 = Config register
+    REG_LOWTHRESH   = 0x02,    // 0000 0010 = Lo_thresh register
+    REG_HITHRESH    = 0x03     // 0000 0011 = Hi_tresh register
+} ads1115_reg_pointer_t;
+
+// Operational status or single-shot conversion start
+// ----------------------------------------------------------------------------
+typedef enum
+{
+    OS_SET_NO_EFFECT      = 0x0000,    // Write 0 = No effect
+    OS_SET_SINGLE_CONV    = 0x8000,    // Write: 1 = Start a single conversion
+    OS_GET_BUSY           = 0x0000,    // Read:  0 = Conversion is performed
+    OS_GET_NOT_BUSY       = 0x8000     // Read:  1 = Conversion is not currently performed
+} ads1115_conf_os_t;
+
+// Input multiplexer configuration
+// ----------------------------------------------------------------------------
+typedef enum
+{
+  MUX_DIFF_0_1 = 0x0000,    // 000 Differential P = AIN0, N = AIN1 (default)
+  MUX_DIFF_0_3 = 0x1000,    // 001 Differential P = AIN0, N = AIN3
+  MUX_DIFF_1_3 = 0x2000,    // 010 Differential P = AIN1, N = AIN3
+  MUX_DIFF_2_3 = 0x3000,    // 011 Differential P = AIN2, N = AIN3
+  MUX_SINGLE_0 = 0x4000,    // 100 Single-ended AIN0
+  MUX_SINGLE_1 = 0x5000,    // 101 Single-ended AIN1
+  MUX_SINGLE_2 = 0x6000,    // 110 Single-ended AIN2
+  MUX_SINGLE_3 = 0x7000     // 111 Single-ended AIN3
+} ads1115_conf_mux_t;
+
+// Programmable gain amplifier configuration
+// ----------------------------------------------------------------------------
+typedef enum
+{
+  PGA_6_144V = 0x0000,  // 000 +/-6.144V range = Gain 2/3
+  PGA_4_096V = 0x0200,  // 001 +/-4.096V range = Gain 1
+  PGA_2_048V = 0x0400,  // 010 +/-2.048V range = Gain 2
+  PGA_1_024V = 0x0600,  // 011 +/-1.024V range = Gain 4
+  PGA_0_512V = 0x0800,  // 100 +/-0.512V range = Gain 8
+  PGA_0_256V = 0x0A00   // 101 +/-0.256V range = Gain 16
+} ads1115_conf_pga_t;
+
+
+// Device operating mode
+// ----------------------------------------------------------------------------
+typedef enum
+{
+   DEV_MODE_CONTIN = 0x0000,
+   DEV_MODE_SINGLE = 0x0100
+} ads1115_conf_mode_t;
+
+// Data rate
+// ----------------------------------------------------------------------------
+typedef enum
+{
+  DR_8SPS   =  0x0000,  // 000   8 samples per second
+  DR_16SPS  =  0x0020,  // 001  16 samples per second
+  DR_32SPS  =  0x0040,  // 010  32 samples per second
+  DR_64SPS  =  0x0060,  // 011  64 samples per second
+  DR_128SPS =  0x0080,  // 100 128 samples per second (default)
+  DR_250SPS =  0x00A0,  // 101 250 samples per second
+  DR_475SPS =  0x00C0,  // 110 475 samples per second
+  DR_860SPS =  0x00E0   // 111 860 samples per second
+} ads1115_conf_dr_t;
+
+
+// Comparator mode
+// ----------------------------------------------------------------------------
+typedef enum
+{
+  CMODE_TRAD    = 0x0000,   // 0 Traditional comparator with hysteresis (default)
+  CMODE_WINDOW  = 0x0010    // 1 Window comparator
+} ads1115_conf_comp_mode_t;
+
+// Comparator polarity
+// ----------------------------------------------------------------------------
+typedef enum
+{
+  CPOL_ACTIVE_LOW = 0x0000,  // ALERT/RDY pin is low when active (default)
+  CPOL_ACTVIVE_HI = 0x0008   // ALERT/RDY pin is high when active
+
+} ads1115_conf_comp_pol_t;
+
+// Latching comparator
+// ----------------------------------------------------------------------------
+typedef enum
+{
+  CLAT_NONLAT = 0x0000,  // Non-latching comparator (default)
+  CLAT_LATCH  = 0x0004   // Latching comparator
+} ads1115_conf_comp_lat_t;
+
+// Comparator queue and disable
+// ----------------------------------------------------------------------------
+typedef enum
+{
+  CQUE_1CONV    = 0x0000,   // Assert ALERT/RDY after one conversions
+  CQUE_2CONV    = 0x0001,   // Assert ALERT/RDY after two conversions
+  CQUE_4CONV    = 0x0002,   // Assert ALERT/RDY after four conversions
+  CQUE_DISABLE  = 0x0003    // Disable the comparator and put ALERT/RDY in high state (default)
+} ads1115_conf_comp_que_t;
+
+
+
+
+class ADS1115
+{
+protected:
+   // Configuration
+   ads1115_I2C_address_t    m_i2cAddress;
+   ads1115_conf_os_t        m_config_os     = OS_SET_NO_EFFECT;
+   ads1115_conf_mux_t       m_config_mux    = MUX_SINGLE_0;
+   ads1115_conf_pga_t       m_config_pga;
+   ads1115_conf_mode_t      m_config_mode   = DEV_MODE_SINGLE;
+   ads1115_conf_dr_t        m_config_dr     = DR_8SPS;
+   ads1115_conf_comp_mode_t m_config_cmode  = CMODE_TRAD;
+   ads1115_conf_comp_pol_t  m_config_cpol   = CPOL_ACTIVE_LOW;
+   ads1115_conf_comp_lat_t  m_config_clat   = CLAT_NONLAT;
+   ads1115_conf_comp_que_t  m_config_cque   = CQUE_DISABLE;
+
+ public:
+  // Addresses for global use
+  static const ads1115_I2C_address_t ADD_GND = ADDRESS_GND;
+  static const ads1115_I2C_address_t ADD_VDD = ADDRESS_VDD;
+  static const ads1115_I2C_address_t ADD_SCL = ADDRESS_SCL;
+  static const ads1115_I2C_address_t ADD_SDA = ADDRESS_SDA;
+/*
+** Set data rate
+*/
+  // Programmable Gain Modes for global use
+  static const ads1115_conf_pga_t GAIN_6_144V = PGA_6_144V;
+  static const ads1115_conf_pga_t GAIN_4_096V = PGA_4_096V;
+  static const ads1115_conf_pga_t GAIN_2_048V = PGA_2_048V;
+  static const ads1115_conf_pga_t GAIN_1_024V = PGA_1_024V;
+  static const ads1115_conf_pga_t GAIN_0_512V = PGA_0_512V;
+  static const ads1115_conf_pga_t GAIN_0_256V = PGA_0_256V;
+
+  ADS1115(ads1115_I2C_address_t i2cAddress = ADDRESS_DEFAULT, ads1115_conf_pga_t config_pga = PGA_6_144V);
+
+  /* Main functions */
+  void      begin(void);
+  int16_t   getCountA0Gnd();
+  int16_t   getCountA1Gnd();
+  int16_t   getCountA2Gnd();
+  int16_t   getCountA3Gnd();
+  int16_t   getCountA0A1();
+  int16_t   getCountA0A3();
+  int16_t   getCountA1A3();
+  int16_t   getCountA2A3();
+  float     getMvPerCount();
+
+  //Utility functions
+  float     convertCountToVolt(int16_t count);
+  void      showConfigRegister();
+  uint16_t  getR2ValueVoltageDivider(float input, float output, uint16_t r1);
+  uint16_t  getR1ValueVoltageDivider(float input, float output, uint16_t r2);
+  float     getShuntForAttenuation(double attenuation, uint16_t pot_size );
+  float     getSerieForAttenuation(double attenuation, uint16_t pot_size );
+
+
+ private:
+  void      setMultiplexer(ads1115_conf_mux_t config_mux);
+  void      setDataRate(ads1115_conf_dr_t config_dr);
+  uint16_t  readRegister(ads1115_reg_pointer_t reg);
+  void      writeRegister(ads1115_reg_pointer_t reg, uint16_t value);
+  int16_t   getConversion();
+  void      triggerConversion();
+  boolean   pollConversion(uint8_t max_retries);
+  boolean   isConversionReady();
+  int16_t   getLastConversionResults();
+  uint16_t  getConfiguration();
+};
